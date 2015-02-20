ad78760a4ca0470bb1df3d367da4cbba8a43a2ed
Mahesh Gouru <gouru.mahesh@charterschool.org>
2015-02-19 19:05:11 -0500

    Added the spacing

--- CmdBasedMOEnarch/src/org/usfirst/frc/team365/robot/MOEMath.java
+++ CmdBasedMOEnarch/src/org/usfirst/frc/team365/robot/MOEMath.java
@@ -3,50 +3,79 @@ package org.usfirst.frc.team365.robot;
 public class MOEMath 
 {
 
+	double norm(double degree)
+	{
+		double result = Math.round(degree)%360;
+		
+		if(result<0)
+		{
+			
+			result=360+result;
+		}
+		
+		return result;
+	}
+	
 
+	double getAngle(double x,double y)
+	{
 
-double norm(double degree){
-double result = Math.round(degree)%360;
-if(result<0){
-result=360+result;
-}
-return result;
-}
-double getAngle(double x,double y){
-double angle = atan2mod(y, x)-90;
-return angle;
-}
-double getPower(double x,double y){
-double power = Math.abs(Math.sqrt(x*x+y*y));
-return limit(power);
-}
-double limit(double current){
-if(current<-1){
-return -1;
-}
-if(current>1){
-return 1;
-}
-return current;
-}
-double sign(double input){
-return input/Math.abs(input);
-}
-double atan2mod(double y, double x){
-double result = Math.atan2(y,x)*180/Math.PI;
-if(result<0){
-result= 360+result;
-}
-return result;
-}
-double turnPower(double input){
-if(Math.abs(input)>3){
-if(Math.abs(input)>20){
-return sign(input)*.3;
-}
-return sign(input)*.1;
-}
-return 0;
-}
+	double angle = atan2mod(y, x)-90;
+		return angle;
+	}
+	
+
+	double getPower(double x,double y)
+	{
+		double power = Math.abs(Math.sqrt(x*x+y*y));
+		return limit(power);
+	}
+
+	double limit(double current)
+	{
+		if(current<-1)
+		{
+			return -1;
+		}
+
+		if(current>1)
+		{
+			return 1;
+		}
+		
+		return current;
+	}
+	
+	double sign(double input)
+	{
+		return input/Math.abs(input);
+	}
+
+	double atan2mod(double y, double x)
+	{
+		double result = Math.atan2(y,x)*180/Math.PI;
+
+		if(result<0)
+		{
+			result= 360+result;
+		}
+		
+		return result;
+	}
+
+	double turnPower(double input)
+	{
+		if(Math.abs(input)>3)
+		{
+			if(Math.abs(input)>20)
+			{
+				return sign(input)*.3;
+			}
+			
+			return sign(input)*.1;
+		}
+		
+		return 0;
+	}
 
 }
